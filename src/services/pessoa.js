074a0ed71376e90dc3ebb7f4e05519e89ba3a311
Danilo Cesar <109434051+daniloz-c@users.noreply.github.com>
2026-05-08 00:55:49 -0300

    Enhance validation and error handling in pessoa service
    Added validation for negative IDs and null values in PegarUm. Included error handling for not found data in Adicionar and PegarTodos methods.

--- src/services/pessoa.js
+++ src/services/pessoa.js
@@ -1,48 +1,79 @@
-const RepositorioExercicio= require("../repositories/pessoa.js")
+const RepositorioExercicio = require("../repositories/pessoa.js")
 
 const repositorio = new RepositorioExercicio()
 class ServicoExercicio {
 
-    async PegarUm(id){
-      if(!id || isNaN(id)) {
-        throw new Error("Favor corretamente o id.")
-      }
-      return repositorio.PegarUm(id)
+  async PegarUm(id) {
+
+
+    //Sugestão de melhoria adicionar uma verificação de numeros negativos em PegarUm
+    //Sugestão de melhoria adicionar uma verificação quando não for informado id
+    if (!id || isNaN(id) || id < -1 || id == null) {
+      throw new Error("Favor corretamente o id.")
+    }
+    // Sugestão de melhoria adicionar um Erro quando o dado não for encontrado
+    const result = await repositorio.PegarUm(id)
+
+    if (result == null) {
+      throw new Error("Dado não encontrado")
+
+    }
+    return repositorio.PegarUm(id)
+  }
+  // Sugestão: Adicionar mensagem de erro caso o banco esteja vazio 
+  async PegarTodos() {
+
+    const result = await repositorio.PegarTodos()
+    console.log(result)
+    if(result.length == 0){
+      throw new Error("Dados não encontrado")
     }
 
-    async PegarTodos(){
-      return repositorio.PegarTodos()
+    return repositorio.PegarTodos()
+  }
+
+  async Adicionar(pessoa) {
+    if (!pessoa) {
+      throw new Error("Favor preencher o pessoa.")
+    } else if (!pessoa.nome) {
+      throw new Error("Favor preencher o nome.")
+      } else if (pessoa.nome.length < 3) {
+    throw new Error("O nome deve ter no mínimo 3 caracteres.")
+      } else if (/\d/.test(pessoa.nome)) {
+    throw new Error("O nome não pode conter números.")
+    } else if (!pessoa.email) {
+      throw new Error("Favor preencher o email.")
+    } else if (!pessoa.senha) {
+      throw new Error("Favor preencher o senha.")
+    }else if (pessoa.senha.length < 8) {
+    throw new Error("A senha deve ter no mínimo 8 caracteres.")
+    }
+    return repositorio.Adicionar(pessoa)
+  }
+
+  async Alterar(id, pessoa) {
+    if (!id || isNaN(id)) {
+      throw new Error("Favor corretamente o id.")
+    }else if (pessoa.nome && /\d/.test(pessoa.nome)) {
+    throw new Error("O nome não pode conter números.")
+  }else if (pessoa.nome && pessoa.nome.length < 3) {
+    throw new Error("O nome deve ter no mínimo 3 caracteres.")
+  }else if (pessoa.senha && pessoa.senha.length < 8) {
+    throw new Error("A senha deve ter no mínimo 8 caracteres.")
+  } else if (/^(.)\1*$/.test(pessoa.senha)) {
+    throw new Error("A senha não pode conter apenas um caractere repetido.")
+  }
+    // Correção: Service alterar pessoa esta chamando adicionar pessoa inves de alterar
+    return repositorio.Alterar(id, pessoa)
+  }
+
+  async Deletar(id) {
+    if (!id || isNaN(id)) {
+      throw new Error("Favor corretamente o id.")
     }
 
-    async Adicionar(pessoa){
-      if(!pessoa) {
-        throw new Error("Favor preencher o pessoa.")
-      } else if(!pessoa.nome) {
-        throw new Error("Favor preencher o nome.")
-      } else if(!pessoa.email) {
-        throw new Error("Favor preencher o email.")
-      } else if(!pessoa.senha) {
-        throw new Error("Favor preencher o senha.")
-      }
-
-      return repositorio.Adicionar(pessoa)
-    }
-
-    async Alterar(id, pessoa){
-      if(!id || isNaN(id)) {
-        throw new Error("Favor corretamente o id.")
-      }
-
-      return repositorio.Alterar(id, pessoa)
-    }
-
-    async Deletar(id){
-      if(!id || isNaN(id)) {
-        throw new Error("Favor corretamente o id.")
-      }
-
-      return repositorio.Deletar(id)
-    }
+    return repositorio.Deletar(id)
+  }
 
 }
 module.exports = ServicoExercicio
